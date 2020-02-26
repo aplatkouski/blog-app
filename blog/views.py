f529e91f7a7d37ad46b70f607116beca603aea7a
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-02-26 11:59:21 +0300

    change blog templates
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.order_by('published_date')[:10]
+	return render(request, 'blog/post_list.html', {'posts': posts})

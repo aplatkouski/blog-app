5e98c89ad41dc0b2c482491190b3b5f611aa6e40
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-02-26 18:11:46 +0300

    add blog/forms
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.http import HttpResponseRedirect, Http404
+from django.contrib.auth.models import User
 
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -13,3 +14,18 @@ def post_list(request):
 def post_detail(request, post_pk):
 	post = get_object_or_404(Post, pk=post_pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', post_pk=post.pk)
+
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})

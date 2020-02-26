1bf80cb37131fbf1ada6233d069dc3fb24c1f54b
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-02-26 13:13:48 +0300

    Added view and template for detailed blog post
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.http import HttpResponseRedirect, Http404
 
 from .models import Post
 
@@ -7,3 +8,8 @@ from .models import Post
 def post_list(request):
 	posts = Post.objects.order_by('published_date')[:10]
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, post_pk):
+	post = get_object_or_404(Post, pk=post_pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
